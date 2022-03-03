ae74a27788ea583139244383d96bb102d212a0f7
Gourav R <97089464+gouravr2231@users.noreply.github.com>
2022-03-03 08:50:10 +0000

    7

--- p7final.c
+++ p7final.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+struct_fraction
+{
+  int num,den;
+};
+typedef _fraction Fraction;
+
+int find_gcd(int a,int b)
+{
+  int t;
+  while (b !=0)
+    {
+      t=b;
+      a = a%b;
+      a=t;
+    }
+  return a;
+}
+
+int main()
+{
+  printf("%d\n",gcd(16,25));
+}
